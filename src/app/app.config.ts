8e4afe8fd2d16a9b2c87453ae533fffc941ad004
Senali-perera <senaliperera1996@gmail.com>
2024-02-02 16:06:28 +0200

    Registered the effect

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, isDevMode} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideStore} from "@ngrx/store";
 import {BookReducer} from "./book/book.reducer";
+import {provideEffects} from "@ngrx/effects";
+import {BookEffects} from "./book/book.effect";
+import {provideStoreDevtools} from "@ngrx/store-devtools";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideStore({ book: BookReducer }),
+    provideEffects(BookEffects),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    })
   ],
 };
