b5dfc2f10f5f4aec55047be6e2345c1572d01c24
Senali-perera <senaliperera1996@gmail.com>
2024-02-02 13:13:51 +0200

    created book-list component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideStore} from "@ngrx/store";
+import {BookReducer} from "./book/book.reducer";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideStore({ book: BookReducer }),
+  ],
 };
